7a40b9df63c8d726bf782beeec8813cec0e5c90b
Kursad <mercolutions@proton.me>
2025-01-29 21:04:15 +0100

    Somewhat improved UserService

--- src/api/src/main/java/com/gainstar/api/service/UserService.java
+++ src/api/src/main/java/com/gainstar/api/service/UserService.java
@@ -18,22 +18,22 @@ public class UserService {
     }
 
     public User getUser(Long id) {
-        return userRepository.findById(id).orElse(null);
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
     }
 
     public User createUser(UserCreationDTO user) {
         try {
-            User existingUsername = this.userRepository.findByUsername(user.username());
-            if (existingUsername != null) {
+            if (userRepository.existsByUsername(user.username())) {
                 throw new Exception("User with username " + user.username() + " already exists");
             }
+
             User newUser = new User();
             newUser.setUsername(user.username());
             // TODO: Hash the password before saving it. We need Spring Security dependency for this.
             newUser.setPassword(user.password());
             newUser.setUserRole(UserRole.USER); // Default to USER role. This seems common practice
-            this.userRepository.save(newUser);
-            return newUser;
+
+            return this.userRepository.save(newUser);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
