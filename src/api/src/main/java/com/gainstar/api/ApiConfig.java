1eff73bab9d05dd2d4f1111a324c10b4ed0ba831
Kursad Mercimek <171381@novacollege.nl>
2025-02-19 16:43:05 +0100

    Added CORS config to API

---
+++ src/api/src/main/java/com/gainstar/api/ApiConfig.java
@@ -0,0 +1,17 @@
+package com.gainstar.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApiConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Voeg OPTIONS toe
+                .allowedHeaders("*") // Toestaan van alle headers
+                .allowCredentials(true); // Indien van toepassing
+    }
+}
